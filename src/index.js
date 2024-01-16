7f7b7cebc5558ba4be8212cb95d7c84123bd0202
DESKTOP-11VLRAP\Nagaraj Hebbar <desktop@123.com>
2024-01-16 20:22:09 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-const config = await fetch("/config.json");
-console.log(await config.json());
+const getconfig = async () => {
+  try {
+    const config = fetch("/config.json");
+    console.log(config.json());
+  } catch (error) {
+    console.log("Error" + error);
+  }
+};
+
 ReactDOM.render(<App />, document.getElementById("root"));
