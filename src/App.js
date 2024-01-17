e2de4bc1e026661ad4156fdf32513e267dda9480
DESKTOP-11VLRAP\Nagaraj Hebbar <desktop@123.com>
2024-01-17 15:25:53 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-
+import configdata from "../src/config.json";
 function App() {
   const value = "World";
   const getconfig = async () => {
     try {
-      const config = await fetch("/config.json");
-      console.log(await config.json());
+      //const config = fetch(configdata);
+      console.log(configdata);
     } catch (error) {
       console.log("Error" + error);
     }
   };
-  console.log(getconfig());
   return <div>Hello {value}</div>;
 }
 
